58b66cfeb15034b272b65d56561f85988b40653e
mamaltse <Maksim.Maltsev@t-systems.com>
2019-03-14 20:03:50 +0300

    first commit

--- src/decoration_validate.py
+++ src/decoration_validate.py
@@ -1,18 +1,16 @@
-import functools
-
-
-def decorator_witharguments(arg):
-    def validate(func):
-        def wrapper(arg, *args, **kwargs):
-            if (low_bound < arg):
-                res = func(arg, *args, **kwargs)
-            else:
-                print("Function call is not valid")
-                return None
-            return res
-        return wrapper
-    return validate
-
-@validate(low_bound = 0)
+@validate(0, 5)
 def set_pixel(pixel_values):
     print("Pixel created!")
+
+def validate(low_bound = 0, upper_bound = 256):
+    def real_decorator(func):
+        def wrapper(*args):
+            pixel = args[0]
+            if (pixel[0:2] < upper_bound and pixel[0:2] < low_bound):
+                res = func(*args)
+            else:
+                print("Function call is not valid")
+                res = None
+            return res
+        return wrapper
+    return real_decorator
